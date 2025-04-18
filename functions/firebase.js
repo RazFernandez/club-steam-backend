aefa3a7175377a77437e2b2ea63b5f9004962398
RazFernandez <mr.fernandez.mx@outlook.com>
2025-04-18 16:32:34 -0500

    chore: restructure project - added firebase.js for Firebase Admin SDK initialization, created firebase-database folder for Firestore-related functions, and moved addUser function to firebase-database/addUser.js

---
+++ functions/firebase.js
@@ -0,0 +1,5 @@
+// functions/firebase.js
+const { initializeApp } = require("firebase-admin/app");
+
+// Inicializa Firebase Admin SDK
+initializeApp();
--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,4 @@
-const { onRequest } = require("firebase-functions/v2/https");
-const { initializeApp } = require("firebase-admin/app");
-const { getFirestore } = require("firebase-admin/firestore");
-
-// Start the firebase SDK
-initializeApp();
-
-// Function to add a user on firestore
-exports.addUser = onRequest(async (req, res) => {
-  const db = getFirestore();
-
-  try {
-    const newUser = {
-      name: "Angel",
-      createdAt: new Date(),
-    };
-
-    // Create the user in the database
-    const docRef = await db.collection("Users").add(newUser);
-
-    // Message to indicate the creation of the user was sucessful.
-    res.status(200).json({ message: `User added with ID: ${docRef.id}` });
-
-  } catch (error) {
-    // Message to indicate the creation of the user failed.
-    console.error("Error adding user:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+// Aquí reexportas las funciones que vayas creando
+module.exports = {
+  ...require("./firebase-database/addUser"),
+};
